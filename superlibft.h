2fc1872207bf3eae2b59c674ec7ead0b19adfc54
Louis Young <youngalou@gmail.com>
2017-06-06 16:06:22 -0700

    Fixed problem with double inclusion.

--- superlibft.h
+++ superlibft.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   superlibft.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyoung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 13:21:39 by lyoung            #+#    #+#             */
-/*   Updated: 2017/06/06 13:23:39 by lyoung           ###   ########.fr       */
+/*   Updated: 2017/06/06 16:05:57 by lyoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef SUPERLIBFT_H
+# define SUPERLIBFT_H
 
 # include "src/libft/libft.h"
 # include "src/get_next_line/get_next_line.h"
